42d67d1cd8d5a72945291e28442b312436f53201
Brian Kinney <briankanokinney@gmail.com>
2019-04-27 18:36:40 -0400

    Improve entry script

--- bin/airtable-to-swagger.js
+++ bin/airtable-to-swagger.js
@@ -1,6 +1,20 @@
+#! /usr/bin/env node
+
 const convert = require('../src/convert');
 
+const fs = require('fs');
+const process = require('process');
 
 
+function main() {
+    const inputFile = process.argv[1];
 
-console.log(JSON.stringify(convert.generateSwaggerObject(schema)));
+    if (inputFile) {
+        const contents = fs.readFileSync(inputFile);
+        const schemaObject = convert.generateSwaggerObject(JSON.parse(contents));
+
+        console.log(JSON.stringify(convert.generateSwaggerObject(schemaObject)));
+    }
+}
+
+main();
