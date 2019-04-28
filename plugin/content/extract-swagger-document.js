8b5b867a1046abb4edb7380227e58c00aed29850
Brian Kinney <briankanokinney@gmail.com>
2019-04-28 16:20:06 -0400

    Remove debug console logging

--- plugin/content/extract-swagger-document.js
+++ plugin/content/extract-swagger-document.js
@@ -1,8 +1,5 @@
 const oasSchema = generateSwaggerObject(window.wrappedJSObject.application);
 
-console.log(`attempting to send ${oasSchema}`);
-browser.runtime.sendMessage({schema: JSON.stringify(oasSchema)}).then(function () {
-    console.log('sent?');
-}).catch(function (e) {
+browser.runtime.sendMessage({schema: JSON.stringify(oasSchema)}).then(function () {}).catch(function (e) {
     console.log(e);
 });
